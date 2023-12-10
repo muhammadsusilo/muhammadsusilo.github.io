ff5b715fb37d55782a2fe24996b9ddf0244edc80
Muhammad Susilo <muhammadsusilo43@gmail.com>
2023-12-10 21:15:05 +0700

    mencoba DOM scroll

--- js/script.js
+++ js/script.js
@@ -5,3 +5,9 @@ const navbar = document.querySelector(".navbar");
 menu.addEventListener("click", function(){
     navbar.classList.toggle("open");
 })
+
+// scroll nav
+const header = document.getElementsByTagName("header")[0]
+window.addEventListener("scroll (0, 13)", function(){
+    header.classList("scrollheader");
+})
